a660bc450927aa16555e1d550ed5989024c1f465
vivek kamdar <vivek.kamdar3@gmail.com>
2021-03-09 13:32:34 +0000

    added

---
+++ Lambda/stop_EC2.py
@@ -0,0 +1,8 @@
+import boto3
+region = 'eu-west-1'
+instances = ['Name of the instance id']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
